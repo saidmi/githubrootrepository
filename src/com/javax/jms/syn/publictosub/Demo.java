7d4941374b46923866ce7f495a94c3dd103b2d92
designModule <123@.com>
2018-02-11 17:49:10 +0800

    新增demo测试add

---
+++ src/com/javax/jms/syn/publictosub/Demo.java
@@ -0,0 +1,10 @@
+package com.javax.jms.syn.publictosub;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("dsfsfsfsfs");
+	}
+
+}
